95a0034c012b352f6dd4d4d2e62d6e9bbf3822bc
Ben T <benjamintang2000@outlook.com>
2025-08-05 23:58:10 -0700

    remediate CKV_AWS_24

--- tf/deployment-role/module/security_group/main.tf
+++ tf/deployment-role/module/security_group/main.tf
@@ -7,13 +7,13 @@ resource "aws_security_group" "ssh_security_group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["64.52.135.238/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["64.52.135.238/0"]
   }
 }
